c1a0bda51a23aa7aee4474b41e61cc663ffb497d
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-01 22:07:10 +0530

    create subreddit channels

---
+++ src/components/community/data/index.js
@@ -0,0 +1,8 @@
+export {default as img1 } from '../../../assets/images/subRedditsImages/image1.png'
+export {default as img2 } from '../../../assets/images/subRedditsImages/image2.jpg'
+export {default as img3 } from '../../../assets/images/subRedditsImages/image3.png'
+export {default as img4 } from '../../../assets/images/subRedditsImages/image4.jpg'
+export {default as img5 } from '../../../assets/images/subRedditsImages/image5.png'
+export {default as img6 } from '../../../assets/images/subRedditsImages/image6.png'
+export {default as img7 } from '../../../assets/images/subRedditsImages/image7.png'
+export {default as img8 } from '../../../assets/images/subRedditsImages/image8.png'
---
+++ src/components/community/data/subreddits.js
@@ -0,0 +1,19 @@
+import { img1,img2,img3,img4,img5,img6,img7,img8 } from "./index"
+export const favorites = [
+    {title:"r/funymore" , num : "156",image:img1},
+    {title:"r/breadkingnews" , num : "12",image:img2},
+    {title:"r/lovestory" , num : "",image:img3},
+    {title:"r/gamingfun" , num : "08",image:img4},
+]
+export const redditFeeds = [
+    {title:"r/moview" , num : "04",image:img5},
+    {title:"r/gaming" , num : "",image:img6},
+    {title:"r/pics" , num : "32",image:img7},
+    {title:"r/gifs" , num : "",image:img8},
+]
+export const community = [
+    {title : "r/funymore" , num : "",image:img1},
+    {title : "r/breadkingnews" , num : "",image:img2},
+    {title : "r/gaming" , num : "43",image:img6},
+    {title : "r/lovestory" , num : "12",image:img3},
+]
