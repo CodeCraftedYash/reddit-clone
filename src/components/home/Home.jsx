3686ec32ec2123ae740aba881a9bea1544e1c561
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-02 23:34:49 +0530

    add three sections and style them
    the sections are Community.jsx Popular.jsx (Advertisement.jsx + Footer.jsx)

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react' ;
 import Community from '../community/Community' ;
 import styles from './Home.module.css' ;
+import Popular from '../popular/Popular';
+import Advertisement from '../ad/Advertisement';
+import Footer from '../footer/Footer';
 function Home() {
   return (
     <div className={styles.homeContainer}>
         <Community />
+        <Popular />
+        <div className={styles.rightSection}>
+          <Advertisement />
+          <Footer />
+        </div>
     </div>
   )
 }
