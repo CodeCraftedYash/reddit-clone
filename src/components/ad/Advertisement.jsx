094b2213d8668f11d959f2ab9505989c93a33286
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-03 16:28:56 +0530

    add advertisement section

--- src/components/ad/Advertisement.jsx
+++ src/components/ad/Advertisement.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import styles from './Advertisement.module.css';
 import adPoster from '../../assets/images/Ads/summerSale.jpg'
+import Logo from '../header/Logo';
+import Button from '../button/Button';
 function Advertisement() {
   return (
     <section className={styles.adContainer}>
         <div className={styles.adPosterContainer}>
-          <img src={adPoster} alt="ad poster" width="100%" height="auto" />
+          <img src={adPoster} alt="ad poster" width="100%" height="auto" className={styles.ad} />
         </div>
-        
+        <div className={styles.adPosterContainer}>
+            <div className={styles.adOnReddit}>
+                <Logo />
+                <h2>Advertise on Reddit</h2>
+                <Button variant="secondary" size="large" >GET STARTED</Button>
+            </div>
+        </div>
     </section>
   )
 }
