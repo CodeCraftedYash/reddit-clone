891570fe3240e132d002be9900c8d6accf215e1d
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-04 21:13:54 +0530

    modify footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,17 +2,25 @@ import React from 'react'
 import styles from './Footer.module.css';
 function Footer() {
   return (
-    <footer className={styles.footer}>
-        <div className={styles.linksContainer}>
-            <ul>
-              <li className={styles.link}><a href="/"></a></li>
+    <footer className={styles.footerContainer}>
+        <div>
+            <ul className={styles.linksContainer}>
+              <li className={styles.link}>About<a href="/"></a></li>
+              <li className={styles.link}>Advertise<a href="/"></a></li>
+              <li className={styles.link}>Reddit App<a href="/"></a></li>
+              <li className={styles.link}>Careers<a href="/"></a></li>
+              <li className={styles.link}>Help<a href="/"></a></li>
+              <li className={styles.link}>Reddit Gold<a href="/"></a></li>
+              <li className={styles.link}>Press<a href="/"></a></li>
+              <li className={styles.link}>Blog<a href="/"></a></li>
+              <li className={styles.link}>Reddit Hits<a href="/"></a></li>
             </ul>
         </div>
         <div className={styles.lastRow}>
-            <h2>&copy;  2020</h2>
-            <div>
-              <h2><a href="">Privacy</a></h2>
-              <h2><a href="">Terms</a></h2>
+            <h2 className={styles.textElement}>&copy;  2020</h2>
+            <div className={styles.privacyTerms}>
+              <h2><a href="/" className={styles.textElement}>Privacy</a></h2>
+              <h2><a href="/" className={styles.textElement}>Terms</a></h2>
             </div>
         </div>
     </footer>
