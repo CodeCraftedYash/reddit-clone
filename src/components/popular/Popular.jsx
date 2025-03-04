d401ce66cd34bc11b0d80067d9ee33952686c26f
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-04 21:23:42 +0530

    add Popular.jsx component  in Home.jsx

---
+++ src/components/popular/Popular.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Popular.module.css';
+import Header from './Header.jsx';
+function Popular() {
+  return (
+    <section className={styles.container}>
+        <Header />
+        <div>
+            {/** rest of the part */}
+        </div>
+    </section>
+  )
+}
+
+export default Popular
