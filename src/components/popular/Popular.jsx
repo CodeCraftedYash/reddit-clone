40d08ca98fd792095b27f83873ad618e8f317cb2
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-05 23:10:32 +0530

    add params to Header component tag to enable sorting feature

--- src/components/popular/Popular.jsx
+++ src/components/popular/Popular.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Popular.module.css';
 import Header from './Header.jsx';
 function Popular() {
+  const [sort, setSort] = useState("hot");
   return (
     <section className={styles.container}>
-        <Header />
-        <div>
-            {/** rest of the part */}
+        <Header sort={sort} setSort={setSort} />
+        <div className={styles.postContainer}>
+            
         </div>
     </section>
-  )
+  ) 
 }
 
 export default Popular
