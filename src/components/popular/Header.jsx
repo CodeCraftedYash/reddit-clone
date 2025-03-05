fd24202884b3b01397f48ed3e23e8c34952810be
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-05 23:32:28 +0530

    modify Header.jsx to accept params and use sorting feature

--- src/components/popular/Header.jsx
+++ src/components/popular/Header.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import styles from './Header.module.css';
-function Header() {
+function Header({ sort , setSort}) {
   return (
     <div className={styles.header}>
             <h1>Popular</h1>
             <nav>
                 <ul className={styles.list}>
-                    <li><a className={styles.popularLink} href=''>Hot</a></li>
-                    <li><a className={styles.popularLink} href=''>New</a></li>
-                    <li><a className={styles.popularLink} href=''>Controversial</a></li>
-                    <li><a className={styles.popularLink} href=''>Rising</a></li>
-                    <li><a className={styles.popularLink} href=''>Top</a></li>
+                    <li className={`${sort=="hot"?styles.activeLink:styles.popularLink}`} onClick={()=>setSort("hot")}>Hot</li>
+                    <li className={`${sort=="new"?styles.activeLink:styles.popularLink}`} onClick={()=>setSort("new")}>New</li>
+                    <li className={`${sort=="controversial"?styles.activeLink:styles.popularLink}`} onClick={()=>setSort("controversial")}>Controversial</li>
+                    <li className={`${sort=="rising"?styles.activeLink:styles.popularLink}`} onClick={()=>setSort("rising")}>Rising</li>
+                    <li className={`${sort=="top"?styles.activeLink:styles.popularLink}`} onClick={()=>setSort("top")}>Top</li>
                 </ul>
             </nav>
         </div>
   )
-}
+} 
 
 export default Header
