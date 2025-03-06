9644c959d8d813476b23b9a9b327412cc54752e5
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-06 23:47:47 +0530

    modify  Post.jsx to render loading and error state

--- src/components/popular/Post.jsx
+++ src/components/popular/Post.jsx
@@ -6,42 +6,14 @@ import { CiShare2 } from "react-icons/ci";
 import { IoIosMore } from "react-icons/io";
 import { FaChevronUp } from "react-icons/fa";
 import { FaChevronDown } from "react-icons/fa";
-function Post({post}) {
+function Post({post,loading,error}) {
     const { title, author, created_utc, thumbnail, num_comments, ups, permalink, num_crossposts } = post;
     const truncText = (text,maxLength=25)=>{
         return text.length > maxLength ? text.slice(0,maxLength) + "..." : text;
     }
     const imageUrl =thumbnail?.startsWith("http") ? thumbnail : null;
-{/* <div className={styles.postContainer}>
-        {imageUrl &&<img src={thumbnail} alt="PostPreview" className={styles.Img} loading='lazy'/>}
-        <div className={styles.containerMainTitle}>
-            <h2 className={styles.text1}>{truncText(title,115)}</h2>
-            <div className={styles.containerAuthorDate}>
-                <h3 className={styles.text2} >{author}</h3>
-                <h3 className={styles.text2}>{formatedDate(created_utc)}</h3>
-            </div>
-        </div>
-        <div className={styles.containerCommentShare}>
-            <div className={styles.containerIconText}>
-            <FiMessageSquare />
-            <h3 className={styles.text2}>{num_comments} Comments</h3>
-            </div>
-            <div className={styles.containerIconText}>
-            <CiShare2 />
-            <h3 className={styles.text2}>{num_crossposts} Share</h3>
-            </div>
-            <div className={styles.containerIconText}>
-            <IoIosMore />
-            <h3 className={`${styles.text2}`}>More</h3>
-            </div>
-            
-        </div>
-        <div className={styles.containerUpvotes}>
-            <FaChevronUp className={styles.arrows}/>
-            <h3>{ups}</h3>
-            <FaChevronDown className={styles.arrows}/>
-        </div>
-    </div> */}
+    if (loading) return <p className={styles.loading}>Loading posts...</p>;
+    if (error) return <p className={styles.error}>Error: {error}</p>;
   return (
     <div className={styles.postContainer}>
         <div className={styles.leftComponent}>
