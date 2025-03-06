bd5aa86045f21b887d60592055da835ab67a4867
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-06 23:07:22 +0530

    modify Popular.jsx to add generate posts using Post.jsx component

--- src/components/popular/Popular.jsx
+++ src/components/popular/Popular.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
 import styles from './Popular.module.css';
 import Header from './Header.jsx';
+import useRedditPosts from '../../hooks/useRedditPosts.jsx';
+import Post from './Post.jsx';
 function Popular() {
   const [sort, setSort] = useState("hot");
+  const subreddit ="indiasocial";
+  const { posts, loading, error } = useRedditPosts(subreddit,sort);
+  console.clear();
+  console.log(posts);
   return (
     <section className={styles.container}>
         <Header sort={sort} setSort={setSort} />
         <div className={styles.postContainer}>
-            
+          {posts.map((item)=>(
+            <Post post={item}/>
+          ))}  
         </div>
     </section>
   ) 
