70175f28d7951cc0b4155f175931694fbb9a06fc
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-06 23:09:35 +0530

    remove unnecessary empty lines from useRedditPost.jsx hook

--- src/hooks/useRedditPosts.jsx
+++ src/hooks/useRedditPosts.jsx
@@ -4,13 +4,12 @@ const useRedditPosts = (subreddit, sort) => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
   useEffect(() => {
     const fetchRedditPosts = async () => {
       try {
         setLoading(true);
         setError(null);
-        const response = await fetch(`https://www.reddit.com/r/${subreddit}/${sort}.json`);
+        const response = await fetch(`https://www.reddit.com/r/${subreddit}/${sort}.json?limit=10`);
         if (!response.ok) throw new Error("Failed to fetch posts");
         const data = await response.json();
         setPosts(data.data.children.map((child) => child.data));
